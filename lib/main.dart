1886041f7faf8ea30a1c141af6e5485582b8caec
rcalix <ramon.calix@laureate.net>
2018-12-18 17:40:13 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+              onPressed: () {},
+              child: Text('Add Product'),
+            ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/food.jpg'),
+                  Text('Food Paradise')
+                ],
+              ),
+            ),
+          ])),
+    );
+  }
+}
